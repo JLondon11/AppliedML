11df524865709db6ae2caa5cdfceeec105a0d601
JLondon11 <129726090+JLondon11@users.noreply.github.com>
2026-09-08 15:04:46 -0400

    Trigger corrected Figure 4 provenance rerun

--- chapters/nlp_part_ii/tokenization_and_representation/figure_04_embedding_geometry/figure_04_embedding_geometry.py
+++ chapters/nlp_part_ii/tokenization_and_representation/figure_04_embedding_geometry/figure_04_embedding_geometry.py
@@ -24,7 +24,7 @@ from huggingface_hub import HfApi
 
 MODEL_ID = "google/bert_uncased_L-2_H-128_A-2"
 HERE = Path(__file__).resolve().parent
-plt.rcParams["svg.fonttype"] = "none"
+plt.rcParams["svg.fonttype"] = "none"\n# provenance-rerun: 128-hidden-dim selector
 
 # Restrained scientific palette.
 PALETTE = {
